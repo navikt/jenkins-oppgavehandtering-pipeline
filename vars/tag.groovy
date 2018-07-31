3182e3bfcd24f0d44355ca372acee29525437fb5
teide <tee@soprasteria.com>
2018-07-31 14:21:21 +0200

    OH-12: Fikset typos for tagging.

--- vars/tag.groovy
+++ vars/tag.groovy
@@ -7,8 +7,8 @@ def call() {
 
     withEnv(['HTTPS_PROXY=http://webproxy-utvikler.nav.no:8088']) {
         withCredentials([string(credentialsId: 'github-token', variable: 'github-token')]) {
-            sh ("tag tag -a ${env.APPLICATION_VERSION} -m ${env.APPLICATION_VERSION}")
-            sh ("tag push https://${github-token}:x-oauth-basic@github.com/navikt/${env.APPLICATION_NAME}.tag --tags")
+            sh ("git tag -a ${env.APPLICATION_VERSION} -m ${env.APPLICATION_VERSION}")
+            sh ("git tag push https://${github-token}:x-oauth-basic@github.com/navikt/${env.APPLICATION_NAME}.tag --tags")
         }
     }
 }
