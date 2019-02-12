834bb4995e9533e80cf9de0aeda5187ca27aea20
Øyvind Roth <oyvind.roth@nav.no>
2019-02-12 09:16:19 +0100

    Correct repo URL.

--- vars/dockerUtils.groovy
+++ vars/dockerUtils.groovy
@@ -11,8 +11,21 @@ def buildAndPush() {
 }
 
 def buildAndPush(final String  applicationName, final String applicationVersion) {
+    
+    def fullDockerImageName = applicationName + ":" + applicationVersion
+    def repoUrl = "https://repo.adeo.no:5443/"
+    docker.withRegistry(repoUrl, "JENKINS_NEXUS_REPO_ADEO_USER") {
+		echo "About to build docker image..."
+		def image = docker.build(repoUrl + fullDockerImageName, "--pull .")
+		echo "About to push the just built docker image..."
+		image.push()
+		echo "The docker image successfully built and pushed."
+	}
+    
+    /*
     def image = docker.build("docker.adeo.no:5000/" + applicationName + ":" + applicationVersion, "--pull .")
     image.push()
+    */
 }
 
 def prune() {
