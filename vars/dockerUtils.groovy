847e34a9def19df456372af04f23558a38362c2c
Øyvind Roth <oyvind.roth@nav.no>
2019-02-12 09:40:42 +0100

    Correct errors

--- vars/dockerUtils.groovy
+++ vars/dockerUtils.groovy
@@ -14,12 +14,12 @@ def buildAndPush(final String  applicationName, final String applicationVersion)
     
     def repoDomainAndPort = "repo.adeo.no:5443/"
     def fullDockerImageName = applicationName + ":" + applicationVersion
-    def dockerTag = "${repoDomainAndPort}" + "${fullDockerImageName}"
+    def dockerTag = repoDomainAndPort + fullDockerImageName
     def repoProtocol = "https://"
     
-    docker.withRegistry("${repoProtocol} + ${repoDomainAndPort}", "JENKINS_NEXUS_REPO_ADEO_USER") {
+    docker.withRegistry(repoProtocol + repoDomainAndPort, "JENKINS_NEXUS_REPO_ADEO_USER") {
         echo "About to build docker image tagged ${dockerTag}..."
-	def image = docker.build("${dockerTag}", "--pull .")
+	def image = docker.build(dockerTag, "--pull .")
 	echo "About to push the just built docker image tagged ${dockerTag} ..."
 	image.push()
 	echo "The docker image successfully built and pushed."
